494fc3669b2775d9a93c9f6447bdb96da767abff
kaniadhea <kaniadhea7@gmail.com>
2024-10-12 22:34:23 +0700

    fungsi copy() untuk membuat salinan objek dari data class. memberikan perubahan pada beberapa properti saat membuat salinan

--- app/src/main/java/com/example/questbasickotlin_0170/dataclass.kt
+++ app/src/main/java/com/example/questbasickotlin_0170/dataclass.kt
@@ -15,7 +15,15 @@ fun main () {
     val data = DataClass(1, "indra@gmail.com")
     println(data)
 
-//Fungsi Equals
+    //Fungsi Equals
     val data2 = DataClass(1, "makan@gmail.com")
     println(data == data2) // false
+
+    // Fungsi Copy
+    val data3 = data.copy()
+    println(data3)
+
+    // Fungsi Copy dengan perubahan
+    val data4 = data.copy( email = "sahar@gmail.com")
+    println(data4)
 }
