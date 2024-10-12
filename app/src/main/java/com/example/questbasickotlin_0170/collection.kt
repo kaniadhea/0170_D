7cc49261b041bd200185e0cfd6f72009bdb54d00
kaniadhea <kaniadhea7@gmail.com>
2024-10-12 21:30:28 +0700

    dengan shape[0] = Oval kita dapat mengubah data pada list mutable dan dengan read only hanya bisa dibaca, bukan dimodifikasi, meskipun sebenarnya referensi tersebut berasal dari list mutable.

--- app/src/main/java/com/example/questbasickotlin_0170/collection.kt
+++ app/src/main/java/com/example/questbasickotlin_0170/collection.kt
@@ -25,4 +25,12 @@ fun ContohList(){
     shape.remove("Triangle")
     println(shape)
 
+    // Mengubah data di dalam list mutable
+    shape[0] = "Oval"
+    println(shape)
+
+    // List Read Only
+    val shapesLocked: List<String> = shape
+    println(shapesLocked)
+
 }
