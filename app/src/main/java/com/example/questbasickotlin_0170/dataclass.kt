f9527800fbf69a766230bb058925edc78cd53d61
kaniadhea <kaniadhea7@gmail.com>
2024-10-12 22:30:43 +0700

    fungsi equals() untuk membandingkan dua objek berdasarkan semua propertinya.

--- app/src/main/java/com/example/questbasickotlin_0170/dataclass.kt
+++ app/src/main/java/com/example/questbasickotlin_0170/dataclass.kt
@@ -9,3 +9,13 @@ data class DataClass(
     val id: Int,
     var email: String
 )
+
+
+fun main () {
+    val data = DataClass(1, "indra@gmail.com")
+    println(data)
+
+//Fungsi Equals
+    val data2 = DataClass(1, "makan@gmail.com")
+    println(data == data2) // false
+}
