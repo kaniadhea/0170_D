38adc130705cf2e4917a767b07e1576a4705249a
kaniadhea <kaniadhea7@gmail.com>
2024-10-12 22:23:23 +0700

    untuk menyimpan dan mengelola data dengan cara yang lebih terstruktur dan efisien.

---
+++ app/src/main/java/com/example/questbasickotlin_0170/dataclass.kt
@@ -0,0 +1,11 @@
+package com.example.questbasickotlin_0170
+
+// Data Class
+// Data class adalah yang digunakan untuk menyimpan data
+// Data class menyediakan fungsi untuk meng-override fungsi equals(), hashcode(), dan toString().
+// Data class juga menyediakan fungsi copy() untuk membuat salinan objek dengan beberapa perubahan.
+
+data class DataClass(
+    val id: Int,
+    var email: String
+)
