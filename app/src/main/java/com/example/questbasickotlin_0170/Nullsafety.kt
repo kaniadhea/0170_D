4f41db694580f4618d4994d330d88176b5e51781
kaniadhea <kaniadhea7@gmail.com>
2024-10-13 09:45:45 +0700

    untuk mencegah kesalahan null pointer dengan memisahkan tipe data menjadi yang dapat bernilai null dan tidak.

---
+++ app/src/main/java/com/example/questbasickotlin_0170/Nullsafety.kt
@@ -0,0 +1,14 @@
+package com.example.questbasickotlin_0170
+
+fun nullSafety() {
+//neverNull has String type
+    var neverNull: String = "This can't be null"
+
+//Throws a compiler error
+//neverNull = null
+
+//nullable has nullable String type
+    var nullable: String? = "You can keep a null her e"
+//This is OK
+    nullable = null
+}
