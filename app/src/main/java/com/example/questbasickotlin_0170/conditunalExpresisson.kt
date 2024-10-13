820b13cc66c2d775f952718d99775c7029bb32dc
kaniadhea <kaniadhea7@gmail.com>
2024-10-13 08:42:05 +0700

    memeriksa apakah nilai variabel number lebih besar dari 10 melacak "Negative number"

---
+++ app/src/main/java/com/example/questbasickotlin_0170/conditunalExpresisson.kt
@@ -0,0 +1,17 @@
+package com.example.questbasickotlin_0170
+
+// Conditional Expression
+// Conditional Expression adalah Ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional Expression menggunakan if-else, when, dan try-catch
+
+fun ConditionalStatement() {
+    println("== Conditional Expression ==")
+    // If-Else
+    val number = 10
+    if (number > 10) {
+        println("Positive Number")
+    }
+    else {
+        println("Negative Number")
+    }
+}
