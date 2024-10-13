427b79231e4a89a51dd26ec5f234e6f5de1c1f8a
kaniadhea <kaniadhea7@gmail.com>
2024-10-13 09:52:52 +0700

    mengakses panjang string dengan mempertimbangkan keamanan null di Kotlin.

--- app/src/main/java/com/example/questbasickotlin_0170/Nullsafety.kt
+++ app/src/main/java/com/example/questbasickotlin_0170/Nullsafety.kt
@@ -1,14 +1,39 @@
 package com.example.questbasickotlin_0170
 
 fun nullSafety() {
-//neverNull has String type
+    //neverNull has String type
     var neverNull: String = "This can't be null"
 
-//Throws a compiler error
-//neverNull = null
+    //Throws a compiler error
+    //neverNull = null
 
-//nullable has nullable String type
+    //nullable has nullable String type
     var nullable: String? = "You can keep a null her e"
-//This is OK
+    //This is OK
     nullable = null
+
+    // Check for null in conditions
+    if (neverNull == null) {
+        println("inferredNonNull is null")
+    } else {
+        println("inferredNonNull is not null")
+    }
+
+    // safe call operator
+    println(neverNull.length) //18
+    println(nullable?.length) // null
+
+    //notNull doesnt accept null value
+    fun strLength(notNull: String): Int {
+        return notNull.length
+    }
+    println(strLength(neverNull)) //18
 }
+
+fun main() {
+    nullSafety()
+}
+
+
+
+
