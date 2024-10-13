9d6e0b8e2a3c5dab86a9fb84594ead6fe6bc77fb
kaniadhea <kaniadhea7@gmail.com>
2024-10-13 08:51:41 +0700

    untuk mencetak "Invalid Number"

--- app/src/main/java/com/example/questbasickotlin_0170/conditunalExpresisson.kt
+++ app/src/main/java/com/example/questbasickotlin_0170/conditunalExpresisson.kt
@@ -27,4 +27,14 @@ fun ConditionalStatement() {
         7 -> println("Saturday")
         else -> println("Invalid Day")
     }
+
+    // Try-Catch
+    val value = "10"
+    try {
+        val result = value.toInt()
+        println("Result: $result")
+    } catch (e: NumberFormatException) {
+        println("Invalid Number")
+    }
+
 }
